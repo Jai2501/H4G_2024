d612f83cb19cfc067274c859da65716dcf2e923b
Jai2501 <52578354+Jai2501@users.noreply.github.com>
2024-02-10 03:29:08 +0800

    Update Dashboard Posts

--- the_sustainability_scorecard/src/Pages/Dashboard.js
+++ the_sustainability_scorecard/src/Pages/Dashboard.js
@@ -8,52 +8,52 @@ import DashboardPost from "../Components/DashboardPost";
 function Dashboard() {
   let sampleData = [
     {
-      name: "John Tan",
+      name: "Tony Tan",
       time: "Today at 7:38 AM",
       location: "Southwest",
-      profilePictureUrl: "DashboardPostAssets/ProfileIcon.png",
-      heading: "Recycled my kitchen waste",
+      profilePictureUrl: "ProfilePictureAssets/BoyOne.jpeg",
+      heading: "Eco-Friendly Commute Challenge",
       content:
-        "Attended GUI’s workshop on recycling last weekend and felt like it’s about time I put thought into action!",
-      badgeUrl: "DashboardPostAssets/BadgeIcon.png",
-      badgeInfo: "John completed the Recycling Race challenge!",
-      uploadedPictureUrl: "DashboardPostAssets/PostPicture.png",
+        "This month, I've decided to bike to work. Not only am I getting fitter, but I'm also cutting down on emissions. Every little bit helps!",
+      badgeUrl: "ChallengesAssets/CarFreeWeekChallenge.png",
+      badgeInfo: "John completed the Car-Free Week challenge!",
+      uploadedPictureUrl: "PostPictureAssets/Transport.jpeg",
     },
     {
       name: "Gareth Wang",
       time: "Today at 10:19 AM",
       location: "Yishun",
-      profilePictureUrl: "DashboardPostAssets/ProfileIcon.png",
+      profilePictureUrl: "ProfilePictureAssets/BoyTwo.jpeg",
       heading: "Visited GUI Site for the first time!!",
       content:
         "Visited GUI for the first time today, and I must tell you, the hype is real! Sustainability, here I come...",
       badgeUrl: "DashboardPostAssets/BadgeIcon.png",
       badgeInfo: "Gareth started the Recycling Race challenge!",
-      uploadedPictureUrl: "DashboardPostAssets/PostPicture.png",
+      uploadedPictureUrl: "PostPictureAssets/GUI.jpeg",
     },
     {
       name: "Vibhusha",
       time: "Today at 11:41 AM",
       location: "Jurong",
-      profilePictureUrl: "DashboardPostAssets/ProfileIcon.png",
+      profilePictureUrl: "ProfilePictureAssets/GirlOne.jpeg",
       heading: "Conservation Volunteering Day",
       content:
         "Spent yesterday volunteering at the local river clean-up crew. It's amazing what a group of dedicated people can accomplish in a day.",
-      badgeUrl: "DashboardPostAssets/BadgeIcon.png",
+      badgeUrl: "ChallengesAssets/ReusableRevolutionChallenge.png",
       badgeInfo: "Vibhusha completed the Conservation Champion challenge!",
-      uploadedPictureUrl: "DashboardPostAssets/PostPicture.png",
+      uploadedPictureUrl: "PostPictureAssets/RiverCleanup.jpeg",
     },
     {
       name: "Jai",
       time: "Today at 11:41 AM",
       location: "Kent Ridge",
-      profilePictureUrl: "DashboardPostAssets/ProfileIcon.png",
+      profilePictureUrl: "ProfilePictureAssets/BoyFour.jpeg",
       heading: "Green Thumb Achievements",
       content:
         "Just finished setting up my first rooftop garden! Excited to see what home-grown veggies can do for my carbon footprint and my cooking.",
-      badgeUrl: "DashboardPostAssets/BadgeIcon.png",
-      badgeInfo: "Jai completed the Rooftop Gardener challenge!",
-      uploadedPictureUrl: "DashboardPostAssets/PostPicture.png",
+      badgeUrl: "ChallengesAssets/CompostingChampionChallenge.png",
+      badgeInfo: "Jai completed the Composting Champion challenge!",
+      uploadedPictureUrl: "PostPictureAssets/Garden.jpg",
     },
   ];
 
