5c3ef5f5395ebf9354913992db8ec93e5338790a
Jai2501 <52578354+Jai2501@users.noreply.github.com>
2024-02-09 22:00:20 +0800

    Update App.js

--- the_sustainability_scorecard/src/App.js
+++ the_sustainability_scorecard/src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
+
+import LoginPage from "./Pages/LoginPage";
+import Dashboard from "./Pages/Dashboard";
 
 function App() {
+  // const location = useLocation();
+  // const showBottomNav = ["/dashboard"].includes(location.pathname);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <div className="App">
+                <header className="App-header">
+                  <img src={logo} className="App-logo" alt="logo" />
+                  <p>
+                    Edit <code>src/App.js</code> and save to reload.
+                  </p>
+                  <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    Learn React
+                  </a>
+                </header>
+              </div>
+            }
+          />
+          <Route path="/login" element={<LoginPage />} />
+
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="*" element={<p>404!</p>} />
+        </Routes>
+      </BrowserRouter>
+      {/* {showBottomNav && <BottomTabBar />} */}
     </div>
   );
 }
