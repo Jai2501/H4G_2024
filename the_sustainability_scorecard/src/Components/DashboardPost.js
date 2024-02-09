843e1491fbbfc61457f17dd100b2974bafc6e5d1
Jai2501 <52578354+Jai2501@users.noreply.github.com>
2024-02-10 00:39:36 +0800

    Add Dashboard Page

---
+++ the_sustainability_scorecard/src/Components/DashboardPost.js
@@ -0,0 +1,77 @@
+import React from "react";
+import "./DashboardPost.css";
+
+// "DashboardPostAssets/ProfileIcon.png"
+// "DashboardPostAssets/BadgeIcon.png"
+// "DashboardPostAssets/PostPicture.png"
+
+function DashboardPost({
+  name,
+  time,
+  location,
+  profilePictureUrl,
+  heading,
+  content,
+  badgeUrl,
+  badgeInfo,
+  uploadedPictureUrl,
+}) {
+  return (
+    <div className="dashboard-post">
+      <div className="info-section">
+        <div className="info">
+          <img src={profilePictureUrl} alt="profile-pic"></img>
+          <div className="name-location-time">
+            <div className="name">{name}</div>
+            <div className="time-location">
+              {time} · {location}
+            </div>
+          </div>
+        </div>
+        <div className="settings">
+          <img
+            className=""
+            src="DashboardPostAssets/SettingsIcon.png"
+            alt="icon"
+          ></img>
+        </div>
+      </div>
+      <div className="post-details">
+        <div className="post-heading">{heading}</div>
+        <div className="post-content">{content}</div>
+      </div>
+      <div className="challenge-info">
+        <img className="" src={badgeUrl} alt="badge-icon"></img>
+        <p>{badgeInfo}</p>
+      </div>
+      <div className="post-picture">
+        <img src={uploadedPictureUrl} alt="holder"></img>
+      </div>
+      <div className="post-like-comment-share">
+        <div className="post-like">
+          <img
+            className=""
+            src="DashboardPostAssets/ThumbsUpIcon.png"
+            alt="icon"
+          ></img>
+        </div>
+        <div className="post-comment">
+          <img
+            className=""
+            src="DashboardPostAssets/CommentIcon.png"
+            alt="icon"
+          ></img>
+        </div>
+        <div className="post-share">
+          <img
+            className=""
+            src="DashboardPostAssets/ShareIcon.png"
+            alt="icon"
+          ></img>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default DashboardPost;
--- the_sustainability_scorecard/src/Pages/Dashboard.js
+++ the_sustainability_scorecard/src/Pages/Dashboard.js
@@ -3,11 +3,79 @@ import "./Dashboard.css";
 
 import DashboardNavigationBar from "../Components/DashboardNavigationBar";
 import BottomTabBar from "../Components/BottomTabBar";
+import DashboardPost from "../Components/DashboardPost";
 
 function Dashboard() {
+  let sampleData = [
+    {
+      name: "John Tan",
+      time: "Today at 7:38 AM",
+      location: "Southwest",
+      profilePictureUrl: "DashboardPostAssets/ProfileIcon.png",
+      heading: "Recycled my kitchen waste",
+      content:
+        "Attended GUI’s workshop on recycling last weekend and felt like it’s about time I put thought into action!",
+      badgeUrl: "DashboardPostAssets/BadgeIcon.png",
+      badgeInfo: "John completed the Recycling Race challenge!",
+      uploadedPictureUrl: "DashboardPostAssets/PostPicture.png",
+    },
+    {
+      name: "Gareth Wang",
+      time: "Today at 10:19 AM",
+      location: "Yishun",
+      profilePictureUrl: "DashboardPostAssets/ProfileIcon.png",
+      heading: "Visited GUI Site for the first time!!",
+      content:
+        "Visited GUI for the first time today, and I must tell you, the hype is real! Sustainability, here I come...",
+      badgeUrl: "DashboardPostAssets/BadgeIcon.png",
+      badgeInfo: "Gareth started the Recycling Race challenge!",
+      uploadedPictureUrl: "DashboardPostAssets/PostPicture.png",
+    },
+    {
+      name: "Vibhusha",
+      time: "Today at 11:41 AM",
+      location: "Jurong",
+      profilePictureUrl: "DashboardPostAssets/ProfileIcon.png",
+      heading: "Conservation Volunteering Day",
+      content:
+        "Spent yesterday volunteering at the local river clean-up crew. It's amazing what a group of dedicated people can accomplish in a day.",
+      badgeUrl: "DashboardPostAssets/BadgeIcon.png",
+      badgeInfo: "Vibhusha completed the Conservation Champion challenge!",
+      uploadedPictureUrl: "DashboardPostAssets/PostPicture.png",
+    },
+    {
+      name: "Jai",
+      time: "Today at 11:41 AM",
+      location: "Kent Ridge",
+      profilePictureUrl: "DashboardPostAssets/ProfileIcon.png",
+      heading: "Green Thumb Achievements",
+      content:
+        "Just finished setting up my first rooftop garden! Excited to see what home-grown veggies can do for my carbon footprint and my cooking.",
+      badgeUrl: "DashboardPostAssets/BadgeIcon.png",
+      badgeInfo: "Jai completed the Rooftop Gardener challenge!",
+      uploadedPictureUrl: "DashboardPostAssets/PostPicture.png",
+    },
+  ];
+
   return (
     <div className="dashboard">
       <DashboardNavigationBar />
+
+      {sampleData.map((item, index) => (
+        <DashboardPost
+          key={index}
+          name={item.name}
+          time={item.time}
+          location={item.location}
+          profilePictureUrl={item.profilePictureUrl}
+          heading={item.heading}
+          content={item.content}
+          badgeUrl={item.badgeUrl}
+          badgeInfo={item.badgeInfo}
+          uploadedPictureUrl={item.uploadedPictureUrl}
+        />
+      ))}
+
       <BottomTabBar />
     </div>
   );
