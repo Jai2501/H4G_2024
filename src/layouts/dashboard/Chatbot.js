3c163d88fbf1cef534b4a4dabe3d026e36f1478a
ViSV29 <141602675+ViSV29@users.noreply.github.com>
2024-02-10 22:52:10 +0800

    Update Chatbot.js

--- src/layouts/dashboard/Chatbot.js
+++ src/layouts/dashboard/Chatbot.js
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import '@chatscope/chat-ui-kit-styles/dist/default/styles.min.css';
 import { MainContainer, ChatContainer, MessageList, Message, MessageInput, TypingIndicator } from '@chatscope/chat-ui-kit-react';
 
+// "Subsitute your own openAI key here"
 const API_KEY = "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";
 // "Explain things like you would to a 10 year old learning how to code."
 const systemMessage = { //  Explain things like you're talking to a software professional with 5 years of experience.
